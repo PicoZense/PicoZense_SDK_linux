96d9176de15cdb8193feea52282b0ddf903cbb94
neal.shen <neal.shen@picovr.com>
2020-04-02 13:27:23 +0800

    V3.0.0.7 update .sh

--- Ubuntu18.04/install.sh
+++ Ubuntu18.04/install.sh
@@ -14,5 +14,5 @@ cd $ORIG_PATH
 if [ "`uname -s`" != "Darwin" ]; then
     # Install UDEV rules for USB device
     cp ${SCRIPT_PATH}/0660-picozense-usb.rules /etc/udev/rules.d/0660-picozense-usb.rules
-    cp -d ${SCRIPT_PATH}/Thirdparty/opencv-3.4.1/lib/x64/* /usr/lib/x86_64-linux-gnu/
+    cp -d ${SCRIPT_PATH}/Thirdparty/opencv-3.4.1/lib/* /usr/lib/x86_64-linux-gnu/
 fi
